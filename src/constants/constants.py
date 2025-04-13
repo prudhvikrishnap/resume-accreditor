2f08c24b8bbaf1b047292f246179f89891f15fc8
Prudhvi Krishna Paruchuri <56634854+prudhvikrishnap@users.noreply.github.com>
2025-04-13 09:10:58 -0500

    Adding logic to retrieve job details

---
+++ src/constants/constants.py
@@ -0,0 +1,3 @@
+CORE_PROGRAMMING_LANGUAGES_LIST = ["python", "java", "c++", "c#"]
+JS_FRONTEND_FRAMEWORK_LIST = ["angular", "react", "vue", "next", "svelte", "ember", "backbone"]
+JS_BACKEND_FRAMEWORK_LIST = ["node", "express", "nest", "meteor", "sail"]
