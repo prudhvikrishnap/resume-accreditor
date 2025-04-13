3e70bcf74d344d239a4f23fdb2caafb9b99f68a6
Prudhvi Krishna Paruchuri <56634854+prudhvikrishnap@users.noreply.github.com>
2025-04-13 09:12:52 -0500

    Adding logic to retrieve job details (#3)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+from constants.constants import *
+
+job_description_dict = {
+    "front_end": [],
+    "back_end": [],
+    "programming_languages": []
+}
+job_description_details = st.text_area("Add Job Description Details")
+
+for word in job_description_details.lower().split():
+    if(word in JS_FRONTEND_FRAMEWORK_LIST):
+        job_description_dict["front_end"].append(word)
+    elif(word in JS_BACKEND_FRAMEWORK_LIST):
+        job_description_dict["back_end"].append(word)
+    elif(word in CORE_PROGRAMMING_LANGUAGES_LIST):
+            job_description_dict["programming_languages"].append(word)
+st.write(job_description_dict)
 
 st.write("Resume Accreditor")
 uploaded_file = st.file_uploader("Upload your resume here", type="pdf")
